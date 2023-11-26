679685d191f27cf3ed86a9f2802f927c7dbfc69c
Dustin Caravaglia <dustin_caravaglia@Dustins-MacBook-Air.local>
2023-11-26 10:43:07 -0500

    commented out line on app.py, flask dev server is up and running

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def index():
+#@app.route('/')
+#def index():
+    #return render_template('index.html')
+
+@app.route('/api/')
+def api_index():
     return render_template('index.html')
 
 if __name__ == '__main__':
