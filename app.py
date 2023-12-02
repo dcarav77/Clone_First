223a7ff27bb5c202c0279f204d85fa0e485f0baf
Dustin Caravaglia <dustin_caravaglia@Dustins-MacBook-Air.local>
2023-12-02 08:51:02 -0500

    edited app.py and added stripe register routes

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import stripe_routes
+from flask_cors import CORS
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='react_app/build')
 
-#@app.route('/')
-#def index():
-    #return render_template('index.html')
+CORS(app)
+
+# Register Stripe routes
+stripe_routes.register_stripe_routes(app)
+
+@app.route('/')
+def index():
+    return "Welcome to my Flask app!"
 
 @app.route('/api/')
 def api_index():
     return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
