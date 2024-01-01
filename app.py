1c66f58ca37e1cd94793950d29e3faf8e0365750
Dustin Caravaglia <dustin_caravaglia@Dustins-Air.lan>
2024-01-01 14:53:28 -0500

    erased some console logs, deleted 12 hook, but api key is exposed on stripe, have to see if its gone now

--- app.py
+++ app.py
@@ -2,25 +2,23 @@ import os
 from flask import Flask, render_template, send_from_directory, jsonify
 from flask_cors import CORS
 import stripe
-#from checkout import process_info
+
 import two_stripe_routes
-from thirteen_hook import register_webhook_routes #hook versions
+from thirteen_hook import register_webhook_routes 
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv('/Users/dustin_caravaglia/Documents/Clone_First/sendgrid.env')
 
 endpoint_secret = os.getenv('STRIPE_ENDPOINT_SECRET')
-print(f"Endpoint Secret: {endpoint_secret}")  # For debugging
+print(f"Endpoint Secret: {endpoint_secret}")  
 
 
 app = Flask(__name__, template_folder='templates', static_folder='react_app/build')
 CORS(app)
 
-# Stripe and SendGrid configuration
 
-#stripe.api_key = os.getenv('STRIPE_API_KEY') #I had to hardcode it to work
-stripe.api_key = 'sk_test_51O7mnXEBiprZstxkH0Lhrcv9OGoe1ZiSOHANZvv8L8kICroOFyzVidtNYAau6C3LFtdZAtNy1xStifVvIyKPYAlS00fsbC5wi7'
+stripe.api_key = os.getenv('STRIPE_API_KEY')
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 
